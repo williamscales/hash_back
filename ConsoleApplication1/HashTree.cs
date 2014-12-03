b94bd855d0ae15da5e326e59d83b483457602fc9
William Scales <william.scales@zzw.ca>
2014-12-02 21:19:27 -0800

    added first stab at implementation of a merkle tree in HashTree.cs

---
+++ ConsoleApplication1/HashTree.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Security.Cryptography;
+using System.Text;
+
+namespace HashBack
+{
+    public class HashTree
+    {
+        private class Node<T>
+        {
+            public Node<T> Left { get; set; }
+            public Node<T> Right { get; set; }
+            public T Value { get; set; }
+
+            public Node() { }
+            public Node(T data) : this(data, null, null) { }
+            public Node(T data, Node<T> left, Node<T> right)
+            {
+                Left = left;
+                Right = right;
+                Value = data;
+            }
+        }
+
+        private Node<byte[]> tree = null;
+
+        public string RootHash
+        {
+            get { return BitConverter.ToString(tree.Value).Replace("-", ""); }
+        }
+
+	    public HashTree(List<byte[]> hashes)
+	    {
+            // The number of leaf nodes needed is ceil(log2(N)), i.e. the
+            // smallest power of 2 >= N where N is the number of hashes
+            var N = hashes.Count;
+            var width = Math.Ceiling(Math.Log(N, 2));
+
+            // We build the tree from the bottom up, by making a node for each
+            // hash. We put these nodes in a queue which we later process two
+            // at a time to build the tree.
+            var nodes = new Queue<Node<byte[]>>();
+            SHA256 hasher = new SHA256Managed();
+            foreach (byte[] hash in hashes)
+            {
+                var node = new Node<byte[]>(hash);
+                nodes.Enqueue(node);
+            }
+
+            // Make sure there are enough leaf nodes to fill the tree
+            if (nodes.Count < width)
+            {
+                var zeroBytes = new byte[] { 0, 0, 0, 0 };
+                var zeroHash = hasher.ComputeHash(zeroBytes);
+                var zeroNode = new Node<byte[]>(zeroHash);
+
+                while (nodes.Count < width) { nodes.Enqueue(zeroNode); }
+            }
+
+            // Process the nodes two at a time to build the tree
+            while (nodes.Count > 1)
+            {
+                var first = nodes.Dequeue();
+                var second = nodes.Dequeue();
+
+                // We hash the concatenation of the two nodes' values and store
+                // that in the parent node.
+                var length = first.Value.Length + second.Value.Length;
+                var toHash = new byte[length];
+                for (int i=0; i < first.Value.Length; i++)
+                {
+                    toHash[i] = first.Value[i];
+                }
+                for (int i=0; i < second.Value.Length; i++)
+                {
+                    toHash[first.Value.Length + i] = second.Value[i];
+                }
+                var hash = hasher.ComputeHash(toHash);
+                var parent = new Node<byte[]>(hash, first, second);
+                nodes.Enqueue(parent);
+            }
+            // The last node in the queue is the root
+            tree = nodes.Dequeue();
+	    }
+    }
+}
--- ConsoleApplication1/Main.cs
+++ ConsoleApplication1/Main.cs
@@ -1,15 +1,32 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Security.Cryptography;
 using System.Text;
 using System.Threading.Tasks;
+
 
 namespace HashBack
 {
     class MainClass
     {
-        static void Main(string[] args)
+        static int Main(string[] args)
         {
+            // Simple example which takes strings as command line arguments
+            // and hashes them.
+            var myStrings = new string[] { "foo", "bar", "baz", "qux" };
+            SHA256 hasher = new SHA256Managed();
+            var hashes = new List<byte[]>();
+            foreach (var myString in myStrings)
+            {
+                var toHash = Encoding.UTF8.GetBytes(myString);
+                hashes.Add(hasher.ComputeHash(toHash));
+            }
+            var tree = new HashTree(hashes);
+            Console.WriteLine(tree.RootHash);
+            Console.ReadLine();
+
+            return 0;
         }
     }
 }
