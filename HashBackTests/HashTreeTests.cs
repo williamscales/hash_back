263f8e07981289a111b1f71f0c965b5e3e1be14f
William Scales <william.scales@zzw.ca>
2014-12-02 22:31:05 -0800

    added a test class for unit tests

---
+++ HashBackTests/HashTreeTests.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using HashBack;
+
+namespace HashBackTests
+{
+    [TestClass]
+    public class HashTreeTests
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
